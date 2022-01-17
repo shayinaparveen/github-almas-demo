9ed8e8867abbbaa9c82b82c4f54e76d7d822da27
Almas Mohamed <asfarashayina@gmail.com>
2022-01-17 20:30:27 +0530

    third commit

--- demo-git.py
+++ demo-git.py
@@ -2,3 +2,4 @@
 C:\Users\ashaikha\pythonProject1\pythonProject1\git-dmo
 C:\Users\ashaikha\pythonProject1\pythonProject1\git-dmo
 C:\Users\ashaikha\pythonProject1\pythonProject1\git-dmo
+git restore
